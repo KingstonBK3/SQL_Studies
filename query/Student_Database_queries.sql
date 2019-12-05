71c639831f6aaaf7fdc7fc6da6fe0ecf85d0184d
pupil <pupil@ivkhk.ee>
2019-12-05 15:09:42 +0200

    Дописать check

--- query/Student_Database_queries.sql
+++ query/Student_Database_queries.sql
@@ -5,11 +5,12 @@ select * from Speciality
 
 create table Speciality(id_speciality int not null identity(10678,1),name nchar(50))
 
-drop table	Groups
+drop table	Learning
 
-create table Groups(id_group varchar(8),school varchar(50) default 'Basic',education_lang varchar(50) default 'Russian',groupreg_date date default getdate(),id_speciality int)
+create table Groups(id_group varchar(8),school varchar(50) default 'Basic',education_lang varchar(50) default 'Russian',groupreg_date date,id_speciality int)
 
-create table Learning(student varchar(50),id_group varchar(8),edustart_date date,eduend_date date,edustatus varchar(50))
+create table Learning(student varchar(50),id_group varchar(8),edustart_date date,eduend_date date,edustatus varchar(50) default 'Learning',
+check(edustatus in ('finished school ','dismissed')))--Прописать
 
 
 --EXEC sp_rename 'Specialty', 'Speciality'
